3eb3925c92a3ab19129246ae0915cca33d23b7df
unknown <kinchoily@hotmail.com>
2021-07-28 00:11:38 +0100

    set up conditional template for hourly forecast to show all hours or current hours onwards depending on present day

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -3,31 +3,33 @@ import './WeatherCard.scss';
 
 const WeatherCard = () => {
 
-    //
     const [weather, setWeather] = useState(null); //api data object
     const [weekday, setweekday] = useState(null); //object properties - weather.forcaste.forecastday[current].date
-    const [condition, setCondition] = useState(null)
+    const [condition, setCondition] = useState(null);
     const [icon, setIcon] = useState(null);
     const [maxTemp, setMaxTemp] = useState(null);
     const [rain, setRain] = useState(null);
 
 
     const [current, setCurrent] = useState(0);//change the array index inside of weather.forecast.forecasteday[current] when user selects between different days
+    const [showHours, setshowHours] = useState(null);// we will use this to show us the hours array inside of weather.forecaste.forecasteday[current].hours
 
-
+    const locationKey = 'London'; //api location
 
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let d = new Date(weekday); // weekday = weather.forecast.forecaste[current].date
 
+    let localtime = new Date(); //we are going to use the local device time for splicing through an array 
+
     const handleClick = (index) => {
         setCurrent(index);
         setweekday(weather.forecast.forecastday[index].date);
-
     }
+
 
     useEffect(() => {
 
-        fetch('http://api.weatherapi.com/v1/forecast.json?key=ef6993d763d541b4812225535211807&q=London&days=5&aqi=no&alerts=no')
+        fetch(`http://api.weatherapi.com/v1/forecast.json?key=ef6993d763d541b4812225535211807&q=${locationKey}&days=5&aqi=no&alerts=no`)
             .then(res => {
                 if (!res.ok) {
                     throw Error('Error could not fetch data for that resourse'); //fetch and handle any errors
@@ -43,12 +45,27 @@ const WeatherCard = () => {
                 setIcon(data.forecast.forecastday[current].day.condition.icon);
                 setMaxTemp(data.forecast.forecastday[current].day.maxtemp_c)
                 setRain(data.forecast.forecastday[current].day.daily_chance_of_rain)
-                console.log(weekday)
 
+                // console.log(d.getHours(weekday))
+                console.log("useEffect")
+                console.log(d.toLocaleString('en-UK', { hour: 'numeric', hour12: true }))
+
+                if (d.getDate() === localtime.getDate()) { //if the date coming from weather.forecast.forecastday[current].date is equal to devices local time
+                    //we set the value of our state to (weather.forecast.forecasteday[current].hour) we use the slice method to copy a portion of that array and use the value from localtime.getHours() as our starting point 
+                    setshowHours(data.forecast.forecastday[current].hour.slice(localtime.getHours()))
+
+                } else {
+                    setshowHours(data.forecast.forecastday[current].hour.slice(0, 24));
+                }
+                // if (d.getDate() === localtime.getDate()) {
+                //     console.log("return spliced hours here ")
+
+                // } else if (d.getDate() !== localtime.getDate()) {
+                //     console.log("return all hours here")
+                // }
             })
 
-    }, [current])
-
+    }, [current, weekday])
 
     return (
         <div>
@@ -56,9 +73,7 @@ const WeatherCard = () => {
 
                 <div className="w-card">
                     <div className="w-card__main">
-
                         <div className="w-card__current">
-
                             <div className="w-card__currentInfo1">
                                 <h2 className="w-card__Location">{weather.location.name}</h2>
                                 <img className="w-card__icon" src={`${icon}`}></img>
@@ -71,26 +86,24 @@ const WeatherCard = () => {
                                 <p className="w-card__Date">{d.getDay()}/{d.getMonth() + 1}/{d.getFullYear()} </p>
                                 <p className="w-card__maxTemp">Chance of rain {rain}%</p>
                             </div>
-
                         </div>
 
-                        <div className="w-card__slider" >
-                            {weather.forecast.forecastday[current].hour.map((hour, index) => {
-                                let d = new Date(hour.time);
-                                return (
-                                    <div className="w-card__hourInfo" key={index}>  {/* style={{ transform: `translateX(${x}px)` }} */}
+                        {showHours && (
+                            <div className="w-card__slider">
+                                {showHours.map((hour, index) => { //we take our array and map through and return template
+                                    let d = new Date(hour.time);
 
-                                        <h2 className="w-card__hour">{d.getHours()}:00</h2>
-                                        <h3 className="w-card__temp">{hour.temp_c}&#8451;</h3>
-
-                                        <p className="w-card__text">{hour.condition.text}</p>
-                                        <p className="w-card__rain">Rain {hour.chance_of_rain} %</p>
-                                    </div>
-                                )
-                            })}
-                            {/* <button className="w-card__btn w-card__btn--right" onClick={handleRightClick}>right</button>
-                            <button className="w-card__btn w-card__btn--left" onClick={handleLeftClick}>left</button> */}
-                        </div>
+                                    return (
+                                        <div className="w-card__hourInfo" key={index}>
+                                            <h2 className="w-card__hour">{d.toLocaleString('en-UK', { hour: '2-digit', minute: '2-digit', hour12: false })}</h2>
+                                            <h3 className="w-card__temp">{hour.temp_c}&#8451;</h3>
+                                            <p className="w-card__text">{hour.condition.text}</p>
+                                            <p className="w-card__rain">Rain {hour.chance_of_rain} %</p>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )}
 
                         <div className="w-card__sCard">
                             {weather.forecast.forecastday.map((map, index) => {
