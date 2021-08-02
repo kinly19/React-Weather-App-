feebd84039a2b1c2bf9f484203e536946cf91caf
unknown <kinchoily@hotmail.com>
2021-08-02 14:57:41 +0100

    cleaned up markup for WeatherCard.js. Removed states which were only being used for accessing api objects properties. Handled this inside of useEffect hook instead

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -6,32 +6,26 @@ import './WeatherCard.scss';
 const WeatherCard = () => {
 
     const [weather, setWeather] = useState(null); //api data object
-    const [weekday, setweekday] = useState(null); //object properties - weather.forcaste.forecastday[current].date
-    const [condition, setCondition] = useState(null);
-    const [icon, setIcon] = useState(null);
-    const [maxTemp, setMaxTemp] = useState(null);
-    const [rain, setRain] = useState(null);
-
-
-    const [current, setCurrent] = useState(0);//change the array index inside of weather.forecast.forecasteday[current] when user selects between different days
-    const [showHours, setshowHours] = useState(null);// we will use this to show us the hours array inside of weather.forecaste.forecasteday[current].hours
+    const [weekday, setweekday] = useState(null); //object property - res.forcaste.forecastday[current].date
+    const [current, setCurrent] = useState(0);//change the arrays index inside of res.forecast.forecasteday[current] when user selects between different days
+    const [showHours, setshowHours] = useState(null);// we will use this to show us the "hour" array inside of res.forecaste.forecasteday[current].hours
 
     const locationKey = 'London'; //api location
 
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let d = new Date(weekday); // weekday = weather.forecast.forecaste[current].date
+    let d = new Date(weekday); // weekday = res.forecast.forecasteday[current].date. 
 
     let localtime = new Date(); //we are going to use the local device time for splicing through an array 
 
     const handleClick = (index) => {
         setCurrent(index);
-        setweekday(weather.forecast.forecastday[index].date);
+        setweekday(weather.[index].date);
     }
-
 
     useEffect(() => {
 
-        fetch(`http://api.weatherapi.com/v1/forecast.json?key=ef6993d763d541b4812225535211807&q=${locationKey}&days=5&aqi=no&alerts=no`)
+        fetch(`http://api.weatherapi.com/v1/forecast.json?key=ef6993d763d541b4812225535211807&q=${locationKey}&days=5&aqi=no&alerts=no`) //fetch data from api 
+
             .then(res => {
                 if (!res.ok) {
                     throw Error('Error could not fetch data for that resourse'); //fetch and handle any errors
@@ -39,33 +33,35 @@ const WeatherCard = () => {
 
                 return res.json(); //if there are no errors we return json.
             })
-            .then((data) => { //take the data from api url and set it to our weather state setWeather
-                setWeather(data)
-                console.log(data)
-                setweekday(data.forecast.forecastday[current].date);
-                setCondition(data.forecast.forecastday[current].day.condition.text);
-                setIcon(data.forecast.forecastday[current].day.condition.icon);
-                setMaxTemp(data.forecast.forecastday[current].day.maxtemp_c)
 
-                setRain(data.forecast.forecastday[current].day.daily_chance_of_rain)
+            .then((res) => { //we set the weather state by .mapping through our res (api data) and returning our own object(seen below), we only use what we need and not the whole object from api. 
+                setWeather(res.forecast.forecastday.map(fcd => {
+                    return {
+                        date: fcd.date, //our object property values 
+                        min: fcd.day.mintemp_c,
+                        max: fcd.day.maxtemp_c,
+                        avg: fcd.day.avgtemp_c,
+                        avghumidity: fcd.day.avghumidity,
+                        condition: fcd.day.condition.text,
+                        uv: fcd.day.uv,
+                        rain: fcd.day.daily_chance_of_rain,
+                        snow: fcd.day.daily_chance_of_snow,
+                        icon: fcd.day.condition.icon,
+                        hourly: fcd.hour,
+                        sunrise: fcd.astro.sunrise,
+                        sunset: fcd.astro.sunset,
+                        moonrise: fcd.astro.moonrise,
+                        moonset: fcd.astro.moonset
+                    }
+                }))
 
-                // console.log(d.getHours(weekday))
-                // console.log("useEffect")
-                // console.log(d.toLocaleString('en-UK', { hour: 'numeric', hour12: true }))
-
-                if (d.getDate() === localtime.getDate()) { //if the date coming from weather.forecast.forecastday[current].date is equal to devices local time
-                    //we set the value of our state to (weather.forecast.forecasteday[current].hour) we use the slice method to copy a portion of that array and use the value from localtime.getHours() as our starting point 
-                    setshowHours(data.forecast.forecastday[current].hour.slice(localtime.getHours()))
+                if (d.getDate() === localtime.getDate()) { // using the date from (weekday) we check if the date coming from the api data is the same as present (todays)date 
+                    setshowHours(res.forecast.forecastday[current].hour.slice(localtime.getHours())) //for the present day we show hourly forecast from current time onwards
                 } else {
-                    setshowHours(data.forecast.forecastday[current].hour.slice(0, 24));
+                    setshowHours(res.forecast.forecastday[current].hour.slice(0, 24));
                 }
 
-                // if (d.getDate() === localtime.getDate()) {
-                //     console.log("return spliced hours here ")
-
-                // } else if (d.getDate() !== localtime.getDate()) {
-                //     console.log("return all hours here")
-                // }
+                setweekday(res.forecast.forecastday[current].date) //gives us a date value we can pass into d 
             })
 
     }, [current, weekday]);
@@ -78,16 +74,16 @@ const WeatherCard = () => {
                     <div className="w-card__main">
                         <div className="w-card__current">
                             <div className="w-card__currentInfo1">
-                                <h2 className="w-card__Location">{weather.location.name}</h2>
-                                <img className="w-card__icon" src={`${icon}`}></img>
-                                <p className="w-card__Condition">{condition}</p>
-                                <h1 className="w-card__Temp">{maxTemp} &#8451;</h1>
+                                <h2 className="w-card__Location">{locationKey}</h2>
+                                <img className="w-card__icon" src={`${weather[current].icon}`}></img>
+                                <p className="w-card__Condition">{weather[current].condition}</p>
+                                <h1 className="w-card__Temp">{weather[current].max} &#8451;</h1>
                             </div>
 
                             <div className="w-card__currentInfo2">
                                 <h1 className="w-card__Day">{days[d.getDay()]}</h1>
                                 <p className="w-card__Date">{d.getDay()}/{d.getMonth() + 1}/{d.getFullYear()} </p>
-                                <p className="w-card__maxTemp">Chance of rain {rain}%</p>
+                                <p className="w-card__maxTemp">Chance of rain {weather[current].rain}%</p>
                             </div>
                         </div>
 
@@ -111,7 +107,7 @@ const WeatherCard = () => {
                         )}
 
                         <div className="w-card__sCard">
-                            {weather.forecast.forecastday.map((map, index) => {
+                            {weather.map((map, index) => {
 
                                 let d = new Date(map.date); // map.date = weather.forecast.forecaste[].date
                                 const dayName = days[d.getDay()]; // we use the getDay() which would return the index of a day and we use that value to select in our days array what day to show :)
@@ -121,11 +117,11 @@ const WeatherCard = () => {
                                     <DailyForecast
                                         index={index} //index comes from the .map()
                                         handleClick={handleClick}
-                                        dailyIcon={map.day.condition.icon}
+                                        dailyIcon={map.icon}
                                         dailyWeekday={dayName}
-                                        dailyTemp={Math.floor(map.day.maxtemp_c)}
-                                        dailyMin={Math.floor(map.day.mintemp_c)}
-                                        dailyRain={map.day.daily_chance_of_rain}
+                                        dailyTemp={Math.floor(map.max)}
+                                        dailyMin={Math.floor(map.min)}
+                                        dailyRain={map.rain}
                                     />
                                 )
                             })}
