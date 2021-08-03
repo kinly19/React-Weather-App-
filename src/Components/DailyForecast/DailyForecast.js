9c55b7951cf02195375ebcadc83d7cba05acae39
unknown <kinchoily@hotmail.com>
2021-08-03 17:47:24 +0100

    DailyForecast.js update to markup

--- src/Components/DailyForecast/DailyForecast.js
+++ src/Components/DailyForecast/DailyForecast.js
@@ -10,14 +10,14 @@ const DailyForecast = ({ index, handleClick, dailyIcon, dailyWeekday, dailyTemp,
 
             <div className="dailyForecast__contentTop">
                 <h2 className="dailyForecast__weekday">{dailyWeekday}</h2>
-                <p className="dailyForecast__maxTemp">{dailyTemp}&#176;C</p>
+                <p className="dailyForecast__maxTemp">{dailyTemp}</p>
                 <img className="dailyForecast__icon" src={dailyIcon} alt="weather icon" />
             </div>
 
             <div className="dailyForecast__contentBottom">
                 <div className="dailyForecast__info">
                     <h1 className="dailyForecast__infoTitle">Min</h1>
-                    <p className="dailyForecast__infoText">{dailyMin}&#176;C</p>
+                    <p className="dailyForecast__infoText">{dailyMin}</p>
                 </div>
                 <div className="dailyForecast__info">
                     <h1 className="dailyForecast__infoTitle">Rain</h1>
