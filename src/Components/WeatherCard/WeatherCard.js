4de02fe65dd85eeeb2c3d343be702177ab6d3fe4
unknown <kinchoily@hotmail.com>
2021-08-13 21:00:01 +0100

    WeatherCard.js light update to mark up

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import DailyForecastSm from '../DailyForecastSm/DailyForecastSm';
+import Form from '../Form/Form';
 import HourlyForecast from '../HourlyForecast/HourlyForecast';
 import './WeatherCard.scss';
 
@@ -21,6 +22,7 @@ const WeatherCard = () => {
     let lastUpdated = new Date(lastUpdate);
 
     const [error, setError] = useState(null); //use this to output any errors to user
+
     const handleClick = (index) => { //index comes from .map
         setCurrent(index);
     }
@@ -47,7 +49,6 @@ const WeatherCard = () => {
                 if (!res.ok) {
                     throw Error('Error could not fetch data for that resourse'); //fetch and handle any errors
                 };
-
                 return res.json(); //if there are no errors we return json.
             })
 
@@ -88,7 +89,7 @@ const WeatherCard = () => {
 
             .catch(err => { //catch any error
                 setError(err.message);
-                });
+            });
 
     }, [locationKey]);
 
@@ -96,11 +97,11 @@ const WeatherCard = () => {
 
         if (weather && currentWeather) {
 
-                if (d.getDate() === localtime.getDate()) { // using the date from (weekday) we check if the date coming from the api data is the same as present (todays)date 
+            if (d.getDate() === localtime.getDate()) { // using the date from (weekday) we check if the date coming from the api data is the same as present (todays)date 
                 setshowHours(weather[current].hourly.slice(localtime.getHours())) //for the present day we show hourly forecast from current time onwards
-                } else {
+            } else {
                 setshowHours(weather[current].hourly.slice(0, 24));
-                }
+            }
 
             setweekday(weather[current].date); //gives us a date value we can pass into d 
             setLastUpdate(currentWeather.lastupdate);
@@ -109,154 +110,149 @@ const WeatherCard = () => {
     }, [weather, currentWeather, current, weekday]);
 
     return (
-        <div>
+
+        <div className="w-card">
             <div className="w-card__form">
-                <form onSubmit={handleSubmit}>
-                    <label>Enter city</label>
-                    <input
-                        type="sumbit"
-                        required
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                    />
-                    <button>click</button>
-                </form>
+                <Form
+                    handleSubmit={handleSubmit}
+                    error={error}
+                    inputValue={inputValue}
+                    weather={weather}
+                    setInputValue={setInputValue}
+                />
             </div>
-
             {weather, currentWeather && (
+                <div className="w-card__main">
 
-                <div className="w-card">
-                    <div className="w-card__main">
-
-                        <div className="cw">{/* currentWeather container top*/}
-                            <div className="cw__contentTop">
-                                <h1 className="cw__location">{currentWeather.locationName}</h1>
-                                <p className="cw__region">{currentWeather.region}</p>
-                                <div className="cw__contentTd">
-                                    <h1 className="cw__currentTemp">{handleCurrentInfo("temp", "max")}</h1>
-                                    <img className="cw__icon" src={`${weather[current].icon}`}></img>
-                                </div>
-
-                                <h2 className="cw__condition">{weather[current].condition}</h2>
-                                <h2 className="cw__weekday">{d.toDateString()}</h2>
-                                <h2 className="cw__lastUpdate">Last updated {lastUpdated.toUTCString()}</h2>
+                    <div className="cw">{/* currentWeather container top*/}
+                        <div className="cw__contentTop">
+                            <h1 className="cw__location">{currentWeather.locationName}</h1>
+                            <p className="cw__region">{currentWeather.region}</p>
+                            <div className="cw__contentTd">
+                                <h1 className="cw__currentTemp">{handleCurrentInfo("temp", "max")}</h1>
+                                <img className="cw__icon" src={`${weather[current].icon}`}></img>
                             </div>
-                            <div className="cw__contentBottom"> {/* current weather container bottom*/}
 
-                                <div className="cw__info"> {/* inner container*/}
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title"> Max</h2>
-                                        <p className="cw__text">{weather[current].max}</p>
-                                    </div>
+                            <h2 className="cw__condition">{weather[current].condition}</h2>
+                            <h2 className="cw__weekday">{d.toDateString()}</h2>
+                            <h2 className="cw__lastUpdate">Last updated {lastUpdated.toUTCString()}</h2>
+                        </div>
+                        <div className="cw__contentBottom"> {/* current weather container bottom*/}
 
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Min</h2>
-                                        <p className="cw__text">{weather[current].min}</p>
-                                    </div>
-
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Rain</h2>
-                                        <p className="cw__text">{weather[current].rain}%</p>
-                                    </div>
-
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Snow</h2>
-                                        <p className="cw__text">{weather[current].snow}%</p>
-                                    </div>
-
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">UV</h2>
-                                        <p className="cw__text">{weather[current].uv}</p>
-                                    </div>
-
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Humidity</h2>
-                                        <p className="cw__text">{handleCurrentInfo("humidity", "avghumidity")}%</p>
-                                    </div>
-
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Feels like</h2>
-                                        <p className="cw__text">{handleCurrentInfo("feelslike", "feelslike")}</p>
-                                    </div>
+                            <div className="cw__info"> {/* inner container*/}
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title"> Max</h2>
+                                    <p className="cw__text">{weather[current].max}</p>
                                 </div>
 
-                                <div className="cw__info">{/* inner container*/}
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Sunrise</h2>
-                                        <p className="cw__text">{weather[current].sunrise}</p>
-                                    </div>
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Min</h2>
+                                    <p className="cw__text">{weather[current].min}</p>
+                                </div>
 
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Sunset</h2>
-                                        <p className="cw__text">{weather[current].sunset}</p>
-                                    </div>
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Rain</h2>
+                                    <p className="cw__text">{weather[current].rain}%</p>
+                                </div>
 
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Moonrise</h2>
-                                        <p className="cw__text">{weather[current].moonrise}</p>
-                                    </div>
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Snow</h2>
+                                    <p className="cw__text">{weather[current].snow}%</p>
+                                </div>
 
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Moonset</h2>
-                                        <p className="cw__text">{weather[current].moonset}</p>
-                                    </div>
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">UV</h2>
+                                    <p className="cw__text">{weather[current].uv}</p>
+                                </div>
 
-                                    <div className="cw__infoItem">
-                                        <h2 className="cw__title">Moon phase</h2>
-                                        <p className="cw__text">{weather[current].moonphrase}</p>
-                                    </div>
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Humidity</h2>
+                                    <p className="cw__text">{handleCurrentInfo("humidity", "avghumidity")}%</p>
+                                </div>
+
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Feels like</h2>
+                                    <p className="cw__text">{handleCurrentInfo("feelslike", "feelslike")}</p>
+                                </div>
+                            </div>
+
+                            <div className="cw__info">{/* inner container*/}
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Sunrise</h2>
+                                    <p className="cw__text">{weather[current].sunrise}</p>
+                                </div>
+
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Sunset</h2>
+                                    <p className="cw__text">{weather[current].sunset}</p>
+                                </div>
+
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Moonrise</h2>
+                                    <p className="cw__text">{weather[current].moonrise}</p>
+                                </div>
+
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Moonset</h2>
+                                    <p className="cw__text">{weather[current].moonset}</p>
+                                </div>
+
+                                <div className="cw__infoItem">
+                                    <h2 className="cw__title">Moon phase</h2>
+                                    <p className="cw__text">{weather[current].moonphrase}</p>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        {showHours && (
+                    {showHours && (
 
-                            <div className="w-card__slider">
+                        <div className="w-card__slider">
 
-                                {showHours.map((hour, index) => { //we take our array and map through and return template
-                                    let d = new Date(hour.time); //we need this to change where d should get its time data from so we can take just the time 
-                                    return (
-                                        <HourlyForecast
-                                            d={d} hourlyTemp={Math.floor(hour.temp_c)}
-                                            hourlyFeelTemp={hour.feelslike_c}
-                                            hourlyCond={hour.condition.text}
-                                            hourlyRain={hour.chance_of_rain}
-                                        />
-                                    )
-                                })}
-
-                            </div>
-                        )}
-
-                        <div className="w-card__sCard">
-                            {weather.map((map, index) => {
-
-                                let d = new Date(map.date); // map.date = weather.forecast.forecaste[].date
-                                const dayName = days[d.getDay()]; // we use the getDay() which would return the index of a day and we use that value to select in our days array what day to show :)
-
+                            {showHours.map((hour, index) => { //we take our array and map through and return template
+                                let d = new Date(hour.time); //we need this to change where d should get its time data from so we can take just the time 
                                 return (
-
-                                    <DailyForecastSm
-                                        index={index} //index comes from the .map()
-                                        handleClick={handleClick}
-                                        dailyIcon={map.icon}
-                                        dailyWeekday={dayName}
-                                        avgTemp={map.avg}
-                                        dailyTemp={map.max}
-                                        dailyMin={map.min}
-                                        dailyRain={map.rain}
+                                    <HourlyForecast
+                                        d={d} hourlyTemp={Math.floor(hour.temp_c)}
+                                        hourlyFeelTemp={hour.feelslike_c}
+                                        hourlyCond={hour.condition.text}
+                                        hourlyRain={hour.chance_of_rain}
                                     />
                                 )
                             })}
 
                         </div>
+                    )}
+
+                    <div className="w-card__sCard">
+                        {weather.map((map, index) => {
+
+                            let d = new Date(map.date); // map.date = weather.forecast.forecaste[].date
+                            const dayName = days[d.getDay()]; // we use the getDay() which would return the index of a day and we use that value to select in our days array what day to show :)
+
+                            return (
+
+                                <DailyForecastSm
+                                    index={index} //index comes from the .map()
+                                    handleClick={handleClick}
+                                    dailyIcon={map.icon}
+                                    dailyWeekday={dayName}
+                                    avgTemp={map.avg}
+                                    dailyTemp={map.max}
+                                    dailyMin={map.min}
+                                    dailyRain={map.rain}
+                                />
+                            )
+                        })}
 
                     </div>
 
                 </div>
             )}
 
-        </div >
+        </div>
+
+
     )
 
 }
