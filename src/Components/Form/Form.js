ab15495baaa7487e6327f464eb832ba2b39c8d69
unknown <kinchoily@hotmail.com>
2021-08-16 15:02:34 +0100

    Form.js update to markup and import of React icon

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,30 +1,35 @@
+import { FiSearch } from 'react-icons/fi';
+// import { IconContext } from 'react-icons/lib';
 import './Form.scss'
 
 const Form = ({ handleSubmit, error, inputValue, setInputValue, }) => {
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                {!error && (
-                    <div><h2>Enter Location</h2></div>
-                )}
-
-                <label>City/Zip/Postal code </label>
+        <div className="form">
+            <form className="form__content" onSubmit={handleSubmit}>
                 <input
+                    className="form__input"
                     type="sumbit"
+                    placeholder={error ? 'Location Unknown...' : 'Enter City/Zip/Postal Code'}
                     required
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                 />
-                <button>click</button>
-                {error && ( //only show if there are any errors
-                    <div>
-                        {<h2>Location unknown please try again</h2>}
-                        <p>{error}</p>
-                    </div>
-                )}
+                <button className="form__btn">
+                    <span className="form__icon">
+                        {/* <IconContext.Provider value={{ color: 'blue' }}> */} {/* if we wanted to style all icons the same we can use reacts IconContext.provider to do so */}
+                        <FiSearch className='form__icon' />
+                        {/* </IconContext.Provider> */}
+                    </span>
+                </button>
             </form>
-        </div>
 
+            {error && ( //only show if there are any errors
+                <div className="form__errorContent">
+                    <p className="form__errorText">{error}</p>
+                </div>
+            )}
+
+        </div>
     )
 }
 
