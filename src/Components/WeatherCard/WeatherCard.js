afd945bdd087dc1ebc2ef3020832c30452bbd87b
unknown <kinchoily@hotmail.com>
2021-08-17 18:41:46 +0100

    Moved markup and styling for class “cw” to be used as a component instead (CurrentWeather)

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import CurrentWeather from '../CurrentWeather';
 import DailyForecastSm from '../DailyForecastSm/DailyForecastSm';
 import Footer from '../Footer';
 import Form from '../Form/Form';
@@ -129,86 +130,14 @@ const WeatherCard = () => {
             {weather, currentWeather && (
                 <div className="w-card__main">
 
-                    <div className="cw">{/* currentWeather container top*/}
-                        <div className="cw__contentTop">
-                            <h1 className="cw__location">{currentWeather.locationName}</h1>
-                            <p className="cw__region">{currentWeather.region}</p>
-                            <div className="cw__contentTd">
-                                <h1 className="cw__currentTemp">{handleCurrentInfo("temp", "max")}</h1>
-                                <img className="cw__icon" src={`${weather[current].icon}`}></img>
-                            </div>
-
-                            <h2 className="cw__condition">{weather[current].condition}</h2>
-                            <h2 className="cw__weekday">{d.toDateString()}</h2>
-                            <h2 className="cw__lastUpdate">Last updated {lastUpdated.toUTCString()}</h2>
-                        </div>
-                        <div className="cw__contentBottom"> {/* current weather container bottom*/}
-
-                            <div className="cw__info"> {/* inner container*/}
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title"> Max</h2>
-                                    <p className="cw__text">{weather[current].max}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Min</h2>
-                                    <p className="cw__text">{weather[current].min}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Rain</h2>
-                                    <p className="cw__text">{weather[current].rain}%</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Snow</h2>
-                                    <p className="cw__text">{weather[current].snow}%</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">UV</h2>
-                                    <p className="cw__text">{weather[current].uv}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Humidity</h2>
-                                    <p className="cw__text">{handleCurrentInfo("humidity", "avghumidity")}%</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Feels like</h2>
-                                    <p className="cw__text">{handleCurrentInfo("feelslike", "feelslike")}</p>
-                                </div>
-                            </div>
-
-                            <div className="cw__info">{/* inner container*/}
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Sunrise</h2>
-                                    <p className="cw__text">{weather[current].sunrise}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Sunset</h2>
-                                    <p className="cw__text">{weather[current].sunset}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Moonrise</h2>
-                                    <p className="cw__text">{weather[current].moonrise}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Moonset</h2>
-                                    <p className="cw__text">{weather[current].moonset}</p>
-                                </div>
-
-                                <div className="cw__infoItem">
-                                    <h2 className="cw__title">Moon phase</h2>
-                                    <p className="cw__text">{weather[current].moonphrase}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <CurrentWeather
+                        currentWeather={currentWeather}
+                        weather={weather}
+                        handleCurrentInfo={handleCurrentInfo}
+                        current={current}
+                        lastUpdated={lastUpdated}
+                        d={d}
+                    />
 
                     {showHours && (
 
