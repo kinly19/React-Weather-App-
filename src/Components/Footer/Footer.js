ea5da1986fa7097664f7227fa78d1c662f51f69f
unknown <kinchoily@hotmail.com>
2021-08-18 13:04:44 +0100

    added rel attribute (noopener) for added security when opening new browser tab

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="linkBack">
             <div className="linkBack__items">
                 <p>Powered By </p>
-                <a target="_blank" href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"></img></a>
+                <a target="_blank" rel="noopener" href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"></img></a>
             </div>
         </div>
     )
