55e7eeb43da34b10136047b51c5b65a25da200aa
unknown <kinchoily@hotmail.com>
2021-10-19 20:43:27 +0100

    CurrentWeather.js code formatting for better readability

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -8,20 +8,30 @@ const CurrentWeather = ({ currentWeather, weather, handleCurrentInfo, current, l
   const weatherList2 = currentWeatherArr.slice(7, 12)
 
     return (
-        <div className="cw">{/* currentWeather container top*/}
-            <div className="cw__contentTop">
-                <h1 className="cw__location">{currentWeather.locationName}</h1> {/* currentWeather */}
-                <p className="cw__region">{currentWeather.region}</p>{/* currentWeather */}
-                <div className="cw__contentTd">
-                    <h1 className="cw__currentTemp">{handleCurrentInfo("temp", "max")}</h1>
-                    <img className="cw__icon" src={`${weather[current].icon}`} alt="weather-icon"></img> {/*Weather */}
-                </div>
+      <div className="cw"> {/* currentWeather container top*/}
+        <div className="cw__contentTop">
+          <h1 className="cw__location">{currentWeather.locationName}</h1>
+          <p className="cw__region">{currentWeather.region}</p>
 
-                <h2 className="cw__condition">{weather[current].condition}</h2>{/*Weather */}
-                <h2 className="cw__weekday">{d.toDateString()}</h2>
-                <h2 className="cw__lastUpdate">Last updated {lastUpdated.toUTCString()}</h2>
-            </div>
-            <div className="cw__contentBottom"> {/* current weather container bottom*/}
+          <div className="cw__contentTd">
+            <h1 className="cw__currentTemp">
+              {handleCurrentInfo("temp", "max")}
+            </h1>
+            <img
+              className="cw__icon"
+              src={`${weather[current].icon}`}
+              alt="weather-icon"
+            ></img>
+          </div>
+
+          <h2 className="cw__condition">{weather[current].condition}</h2>
+          <h2 className="cw__weekday">{d.toDateString()}</h2>
+          <h2 className="cw__lastUpdate">
+            Last updated {lastUpdated.toUTCString()}
+          </h2>
+        </div>
+
+        <div className="cw__contentBottom"> {/* current weather container bottom*/}
           <div className="cw__info">
             {weatherList1.map((currentInfo) => (
               <CurrentWeatherItems
@@ -30,8 +40,8 @@ const CurrentWeather = ({ currentWeather, weather, handleCurrentInfo, current, l
                 onCurrentInfo={handleCurrentInfo}
               />
             ))}
-                    </div>
-
+          </div>
+          
           <div className="cw__info">
             {weatherList2.map((currentInfo) => (
               <CurrentWeatherItems
@@ -40,17 +50,9 @@ const CurrentWeather = ({ currentWeather, weather, handleCurrentInfo, current, l
                 onCurrentInfo={handleCurrentInfo}
               />
             ))}
-                    </div>
-
-                    <div className="cw__infoItem">
-                        <h2 className="cw__title">Rain</h2>
-                        <p className="cw__text">{weather[current].rain}%</p>
-                    </div>
-
-                    <div className="cw__infoItem">
-                        <h2 className="cw__title">Snow</h2>
-                        <p className="cw__text">{weather[current].snow}%</p>
-                    </div>
+          </div>
+        </div>
+      </div>
     );
 }
 
