d238465bd53947daae6eb76df255a82dac7f79ef
unknown <kinchoily@hotmail.com>
2021-10-19 20:25:46 +0100

    Footer.js code formatting

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,25 @@
-import './Footer.scss'
+import "./Footer.scss";
 
 const Footer = () => {
-    return (
-        <div className="linkBack">
-            <div className="linkBack__items">
-                <p>Powered By </p>
-                <a target="_blank" rel="noopener" href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"></img></a>
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div className="linkBack">
+      <div className="linkBack__items">
+        <p>Powered By </p>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.weatherapi.com/"
+          title="Free Weather API"
+        >
+          <img
+            src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png"
+            alt="Weather data by WeatherAPI.com"
+            border="0"
+          ></img>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
