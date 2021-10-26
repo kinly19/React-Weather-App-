0ce836d9031d711131d674911354a0a1990d0af8
unknown <kinchoily@hotmail.com>
2021-10-26 15:06:15 +0100

    Created fetch-api custom hook

---
+++ src/hooks/fetch-api.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (url, isLocation) => {
+
+  const [data, setData] = useState(null);
+  const [error, setIsError] = useState(null);
+
+  useEffect(() => {
+    if (isLocation) {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            console.log("Error something went wrong");
+            throw Error("Error could not fetch data for that resourse");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsError(null);
+        })
+        .catch((error) => {
+          setIsError(error.message);
+        });
+    }
+  }, [url]);
+
+  return { data, error };
+};
+export default useFetch;
